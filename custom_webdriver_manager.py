1c95213d27a1dd4d8977d7792ccba10ce11c19d4
Gabriel <123216441+Jamalo-TV@users.noreply.github.com>
2024-11-01 23:11:36 +0100

    Update custom_webdriver_manager.py -> wrong file

--- custom_webdriver_manager.py
+++ custom_webdriver_manager.py
@@ -1,43 +1,22 @@
 from selenium import webdriver
-from pathlib import Path
-import json
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
-# Function to load cookies from a specified username in the single data file
-def load_cookies(driver, username_insta):
-    data_file = Path('./cookies_data.txt')
+class WebDriverManager:
+    def __init__(self):
+        self.drivers = {}
 
-    if data_file.exists():
-        with data_file.open('r') as file:
-            # Read each line as a JSON object
-            for line in file:
-                data = json.loads(line.strip())
-                # Find the entry matching the specified username
-                if data.get("username") == username_insta:
-                    for cookie in data.get("cookies", []):
-                        driver.add_cookie(cookie)
-                    print(f"Cookies loaded for username: {username_insta}")
-                    return
-        print(f"No cookies found for username: {username_insta}")
-    else:
-        print("Cookies data file does not exist.")
+    def create_driver(self, session_id):
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--start-maximized")
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+        self.drivers[session_id] = driver
+        return driver
 
-# Initialize the browser
-driver = webdriver.Chrome()
+    def get_driver(self, session_id):
+        return self.drivers.get(session_id)
 
-# Navigate to the target website
-driver.get("https://www.instagram.com/")  # Replace "DOMAIN" with the actual domain, e.g., "https://www.instagram.com/"
-
-# Load cookies for the specified username
-username_insta = "gabriel.haefli"  # Change this to the desired username
-load_cookies(driver, username_insta)
-
-# Refresh the page to apply the cookies
-driver.refresh()
-
-# Continue with your browsing actions
-# ...
-while True:
-    x = input("QUIT? ")
-    if x.upper() == "JA":
-        driver.quit()
-        break
+    def remove_driver(self, session_id):
+        driver = self.drivers.pop(session_id, None)
+        if driver:
+            driver.quit()
